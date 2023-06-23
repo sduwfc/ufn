2ea7ee4b1db4a95bc48f3ed3e9277d3deeb5541d
Kilmu <fuchao.wang@sophgo.com>
2023-06-23 23:48:26 +0800

    fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ufn',
-    version='0.1',
+    version='0.1.1',
     packages=find_packages(),
     description='upload for nas',
     long_description=open('README.md').read(),
